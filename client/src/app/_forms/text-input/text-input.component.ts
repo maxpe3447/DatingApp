d0d060975e259a6a0593d2c5d4a9145bf2209080
maxpe3447 <36305245+maxpe3447@users.noreply.github.com>
2023-12-23 00:29:26 +0200

    fix react-forms

--- client/src/app/_forms/text-input/text-input.component.ts
+++ client/src/app/_forms/text-input/text-input.component.ts
@@ -1,11 +1,11 @@
 import { Component, Input, Self } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ControlValueAccessor, FormControl, NgControl } from '@angular/forms';
+import { ControlValueAccessor, FormControl, NgControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-text-input',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './text-input.component.html',
   styleUrl: './text-input.component.css'
 })
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,22 +1,24 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AbstractControl, FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 import { AccountService } from '../services/account.service';
 import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { TextInputComponent } from "../_forms/text-input/text-input.component";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, FormsModule, ToastrModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
+  imports: [CommonModule, FormsModule, ToastrModule, ReactiveFormsModule, TextInputComponent]
 })
-export class RegisterComponent implements OnInit{
+export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
-  registerForm: FormGroup =new FormGroup({});
+  registerForm: FormGroup = new FormGroup({});
   constructor(private accountService: AccountService,
-    private toastr: ToastrService) {
+    private toastr: ToastrService,
+    private fb: FormBuilder) {
 
   }
   ngOnInit(): void {
@@ -24,23 +26,28 @@ export class RegisterComponent implements OnInit{
   }
 
   initializeFotm() {
-    this.registerForm = new FormGroup({
-      username: new FormControl('', Validators.required),
-      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
-      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')])
+    this.registerForm = this.fb.group({
+      gender: ['male'],
+      username: ['', Validators.required],
+      knownAs: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      confirmPassword: ['', [Validators.required, this.matchValues('password')]]
     });
-    // this.registerForm.controls['password'].valueChanges.subscribe({
-    //   next: ()=> this.registerForm.controls['confirmPassword'].updateValueAndValidity()
-    // })
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: ()=> this.registerForm.controls['confirmPassword'].updateValueAndValidity()
+    })
   }
-  matchValues(matchTo: string):ValidatorFn{
+  matchValues(matchTo: string): ValidatorFn {
     return (control: AbstractControl) => {
-      return control.value === control.parent?.get(matchTo)?.value ? null : {notMatching: true}
+      return control.value === control.parent?.get(matchTo)?.value ? null : { notMatching: true }
     }
   }
   register() {
     console.log(this.registerForm.value);
-    
+
     // this.accountService.register(this.model).subscribe({
     //   next: () => {
     //     this.cancel();
