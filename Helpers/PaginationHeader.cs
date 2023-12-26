5977bdb4208e3bc8a1c50f8474ee56be031b6b78
maxpe3447 <36305245+maxpe3447@users.noreply.github.com>
2023-12-26 22:14:19 +0200

    add paginationHeaderInfo

---
+++ Helpers/PaginationHeader.cs
@@ -0,0 +1,12 @@
+﻿using System.Numerics;
+
+namespace DatingApp.Helpers
+{
+    public class PaginationHeader
+    {
+        public int CurrentPage { get; set; }
+        public int ItemPerPage { get; set; }
+        public int TotalItems { get; set; }
+        public int TotalPages { get; set; }
+    }
+}
