bc7604fcc366507fbb1ce5b0eb3144e7fa201e84
maxpe3447 <36305245+maxpe3447@users.noreply.github.com>
2024-01-05 00:18:37 +0200

    fix database migrations

--- Entities/Message.cs
+++ Entities/Message.cs
@@ -7,6 +7,7 @@
         public string SenderUsername { get; set;}
         public AppUser Sender { get; set; }
         public int RecipientId { get; set; }
+        public string RecipientUsername { get; set; }
         public AppUser Recipient { get; set; }
         public string Content { get; set; }
         public DateTime? DateRead { get; set; }
