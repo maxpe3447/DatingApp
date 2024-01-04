264f8a958cae5dc95073cfcc5c3ff161eeed78be
maxpe3447 <36305245+maxpe3447@users.noreply.github.com>
2024-01-05 00:14:00 +0200

    add MessageDto

---
+++ Services/RepositoryService/IMessageRepository.cs
@@ -0,0 +1,16 @@
+﻿using DatingApp.DTOs;
+using DatingApp.Entities;
+using DatingApp.Helpers;
+
+namespace DatingApp.Services.RepositoryService
+{
+    public interface IMessageRepository
+    {
+        void AddMessage(Message message);
+        void DeleteMessage(Message message);
+        Task<Message> GetMessage(int id);
+        Task<PageList<MessageDto>> GetMessagesForUser();
+        Task<IEnumerable<MessageDto>> GetMessageThread(int currentUserId, int RecipientId);
+        Task<bool> SaveAllAsync();
+    }
+}
