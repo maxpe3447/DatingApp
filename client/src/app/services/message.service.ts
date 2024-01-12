04b4ae3bc5bd41982f81f9b349aa23985cea579e
maxpe3447 <36305245+maxpe3447@users.noreply.github.com>
2024-01-12 23:24:02 +0200

    add sendMessage method to service

--- client/src/app/services/message.service.ts
+++ client/src/app/services/message.service.ts
@@ -21,5 +21,9 @@ export class MessageService {
 
   getMessageThread(username:string){
     return this.http.get<Message[]>(this.baseUrl+'messages/thread/'+username);
-  } 
+  }
+  
+  sendMessage(username:string, content:string){
+    return this.http.post<Message>(this.baseUrl+'messages', {recipientUsername:username, content});
+  }
 }
