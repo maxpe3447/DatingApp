9ac40d821bab154470c0e0d69445c95f8cfab86e
maxpe3447 <36305245+maxpe3447@users.noreply.github.com>
2024-01-20 23:08:25 +0200

    add PhotoForApprovalDto

---
+++ DTOs/PhotoForApprovalDto.cs
@@ -0,0 +1,10 @@
+﻿namespace DatingApp.DTOs
+{
+    public class PhotoForApprovalDto
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public string Username { get; set; }
+        public bool IsApproved { get; set; }
+    }
+}
