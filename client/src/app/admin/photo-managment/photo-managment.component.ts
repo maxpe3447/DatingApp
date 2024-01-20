0e11897b220d3c1cec7c06c516794a2ea8fc4e6f
maxpe3447 <36305245+maxpe3447@users.noreply.github.com>
2024-01-20 23:58:20 +0200

    add 'isApproved' to photo model; release manage photo for admin

--- client/src/app/admin/photo-managment/photo-managment.component.ts
+++ client/src/app/admin/photo-managment/photo-managment.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, numberAttribute } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AdminService } from '../../services/admin.service';
+import { Photo } from '../../model/photo';
 
 @Component({
   selector: 'app-photo-managment',
@@ -8,6 +10,31 @@ import { CommonModule } from '@angular/common';
   templateUrl: './photo-managment.component.html',
   styleUrl: './photo-managment.component.css'
 })
-export class PhotoManagmentComponent {
+export class PhotoManagmentComponent implements OnInit{
+
+  photos:Photo[] = [];
+
+  constructor(private adminService:AdminService){}
+  ngOnInit(): void {
+    this.getPhotosForApproval();
+  }
+
+  getPhotosForApproval(){
+    this.adminService.getPhotoForApproval().subscribe({
+      next: photos=> this.photos = photos
+    })
+  }
+
+  approvalPhoto(photoId: number){
+    this.adminService.approvePhoto(photoId).subscribe({
+      next: ()=>this.photos.splice(this.photos.findIndex(p=>p.id === photoId), 1)
+    })
+  }
+
+  rejectPhoto(photoId: number){
+    this.adminService.rejectPhoto(photoId).subscribe({
+      next: ()=> this.photos.splice(this.photos.findIndex(p=>p.id === photoId, 1))
+    })
+  }
 
 }
