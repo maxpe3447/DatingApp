2ef2629dfc2cb20358997ebad225f50260020982
maxpe3447 <36305245+maxpe3447@users.noreply.github.com>
2024-01-20 22:58:00 +0200

    add Photo table in data context

--- DTOs/PhotoDto.cs
+++ DTOs/PhotoDto.cs
@@ -3,7 +3,7 @@
 public class PhotoDto
 {
     public int Id { get; set; }
-    public String Url { get; set; }
+    public string Url { get; set; }
     public bool IsMain { get; set; }
     public bool IsApproved { get; set; }
 }
