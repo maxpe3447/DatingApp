d2303cee0b5e30c71a9513e8d58d3b1afed5d4f8
maxpe3447 <36305245+maxpe3447@users.noreply.github.com>
2024-01-20 22:51:27 +0200

    add approve check in photo entity

--- Entities/Photo.cs
+++ Entities/Photo.cs
@@ -6,6 +6,7 @@ namespace DatingApp.Entities
     public class Photo
     {
         public int Id { get; set; }
+        public bool IsApproved { get; set; } = false;
         public string Url { get; set; }
         public bool IsMain { get; set; }
         public string PublicId { get; set; }
